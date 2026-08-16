edee5a29db0407777574e71e67b8a3075a6a4489
ankithapawarr26-bit <ankithapawarr26@gmail.com>
2026-08-16 17:42:45 +0530

    Create Grade_calculator.py

---
+++ Grade_calculator.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter your marks"))
+if marks>=90 and marks <=100:
+  print("Congratulations! Grade A")
+elif marks>=70 and marks<90:
+  print("Grade B")
+elif marks>=60 and marks<70:
+  print("Grade C")
+elif marks>=50 and marks<60:
+  print("Grade D")
+else:
+  print("Fail")
