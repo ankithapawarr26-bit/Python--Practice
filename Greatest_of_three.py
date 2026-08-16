0c4d300f96331b4c69a2813f41e53f075219db65
ankithapawarr26-bit <ankithapawarr26@gmail.com>
2026-08-16 17:34:27 +0530

    Create Greatest_of_three.py

---
+++ Greatest_of_three.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+num3=int(input("Enter number 3"))
+if num1>num2 and num1>num3:
+  print(f"{num1} is greatest number")
+elif num2>num1 and num2>num3:
+  print(f"{num2} is greatest number")
+else:
+  print(f"{num3} is greatest number")
